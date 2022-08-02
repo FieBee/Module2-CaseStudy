23de069f546514d0d1d0037cc65accd9837ae6a8
FieBee <phuonglf98@gmail.com>
2022-08-02 11:29:24 +0700

    add account.AccountUser

--- src/account/AccountUser.java
+++ src/account/AccountUser.java
@@ -1,4 +1,43 @@
 package account;
 
+import java.util.ArrayList;
+
 public class AccountUser {
+
+    private String userName;
+    private String userPassword;
+
+    private final ArrayList<AccountUser> accountUserArrayList = new ArrayList<>();
+
+    public AccountUser(String userName, String userPassword) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+    }
+
+    public AccountUser(){}
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public ArrayList<AccountUser> getAccountUserArrayList() {
+        return accountUserArrayList;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("║ %-15s ║ %-15s ║",userName,userPassword);
+    }
 }
